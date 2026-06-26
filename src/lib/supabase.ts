77c67e910ff33a8345fb70f5418210d865480ca2
Alexander Bandholt <alo@MacBook-Air-von-Alexander.local>
2026-06-26 17:44:33 +0200

    Hardcode Supabase credentials for production

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  'https://zgqfoztgzdesamzgpwdk.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpncWZvenRnemRlc2Ftemdwd2RrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0ODA5NDYsImV4cCI6MjA5ODA1Njk0Nn0.9DlMHOLZDym4OyJ9hKZKc8STmBiHZWJGQ34-EQepiyk'
 )
